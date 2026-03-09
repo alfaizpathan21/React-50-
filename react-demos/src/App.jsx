e9fb69a7c6a3a655d565b7306d4158686264a1b0
alfaizpathan21 <alfaiz786pathan@gmail.com>
2026-03-09 12:19:46 +0530

    nothing text

--- react-demos/src/App.jsx
+++ react-demos/src/App.jsx
@@ -3,16 +3,41 @@ import StyledCard from "./components/StyledCard";
 import ProfileCard from "./components/ProfileCard";
 import IconComponent from "./components/IconComponent";
 
- 
+const Button =()=>{
+  const hnadleclick =()=>{ console.log(Math.round(Math.random()*101));
+  }
+  return <button onClick={hnadleclick}>Click</button>
+} 
+
+
+const Copy =()=>{
+
+  const copyHandler=()=>{
+    console.log("stop copying my content")
+  }
+  
+  return(
+    
+    <p onCopy={copyHandler}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Exercitationem ad doloribus voluptatum odit, itaque, sed ea non cupiditate enim, autem aliquam. Blanditiis totam quis consequatur magni quia vero doloremque quisquam!</p>
+  )
+}
+
  const App = () => {
   return(
  
-  <section>
-  <TiShoppingCart />
-  <StyledCard/>
-  <ProfileCard/>
-  <IconComponent/>
-  </section>
+<section>
+   
+  <Copy/>
+</section>
+
+
+
+  // <section>
+  // <TiShoppingCart />
+  // <StyledCard/>
+  // <ProfileCard/>
+  // <IconComponent/>
+  // </section>
    
  
   
