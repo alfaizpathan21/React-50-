10a9edde5f9dceb5de05270778c5982869ad0de8
alfaizpathan21 <alfaiz786pathan@gmail.com>
2026-03-10 20:15:11 +0530

    usestate 2

--- react-demos/src/App.jsx
+++ react-demos/src/App.jsx
@@ -2,23 +2,33 @@
  
  const App = () => {
 
-  const [friend,setFriend]=useState(["omkar","hpn"])
+  const [movie,setMovie]=useState([
+    {id:1,title:"spiderman",rating:3},
+     {id:5,title:"batman",rating:13}
+
+  ])
   
-  const addonefriend=()=>setFriend([...friend,"alfaiz"])
-  const removefriend=()=>setFriend(friend.filter((f)=>f!="hpn"))
-  return (
-      <section>
-        {friend.map(f=>(
-          <li key={Math.random()}>{f}</li>
-        ))}
+  const handlerclick=()=>{
+  setMovie(
+  movie.map((m)=>(m.id==1?{ ...movie,title:"john wick 5"}:m))
+  )
+  }
+   return (
+    
+     <section>
+      {movie.map(m=>(
+        <li key={Math.random()}>{m.title}</li>
+      ))}
 
-
-<button onClick={addonefriend}>Add new friend</button>
-<button onClick={removefriend}>remove friend</button>
-        
-      </section>
+      <button onClick={handlerclick}>Change movie name</button>
+     </section>
    )
  }
  
  export default App
+
+ 
+ 
+ 
+ 
  
